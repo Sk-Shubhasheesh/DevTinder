9b85a346ad3f0ab99cb478d34e8c300fb38ce848
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-03-30 21:16:19 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ app.patch("/user", async(req, res)=>{
     const userId = req.body.userId;
     const data = req.body;
     try{
-        await User.findByIdAndUpdate({_id: userId},data);
+        await User.findByIdAndUpdate({_id: userId},data, {runValidators: true});
         res.send("User Updated Successfully!!");
 
     }catch{
