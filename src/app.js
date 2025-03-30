5d0679619500f797d4c46659ed2cbfa9692a228b
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-03-30 16:40:43 +0530

    Dynamic Signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-const connectDB =  require("./congif/database")
+const connectDB =  require("./congif/database");
 const app = express();
+const User = require('./models/user');
+
+app.use(express.json());
+
+app.post("/signup", async(req, res)=>{
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User Added successfully!");
+    } catch(err){
+        res.status(400).send("Error saving the user:" + err.message);
+    }
+    
+})
+
+
+
 
 connectDB().then(()=>{
     console.log("Database connection established....");
