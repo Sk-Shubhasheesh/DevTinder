c10aa14d37bc1f5a23a1082b52750c5aed864237
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-03-31 09:28:28 +0530

    Schema Validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -14,11 +15,23 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address" +value)
+            }
+
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password: " + value)
+            }
+
+        }
     },
     age: {
         type: Number,
@@ -35,7 +48,14 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
-        default: "https://geographyandyou.com/images/user-profile.png"
+        default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo url" + value)
+            }
+
+        }
+        
     },
     about: {
         type: String,
