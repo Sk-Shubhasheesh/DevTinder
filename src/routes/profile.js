8c3554bada75c562e147d3afdda9b1a40ca64b2f
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-04-04 20:35:24 +0530

    Profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const profileRouter = express.Router();
 const {userAuth} = require('../middlewares/auth');
+const { validateProfileEditData } = require("../utils/validation");
 
-
-// GET Profile APi
-profileRouter.get("/profile",userAuth, async (req, res) => {
+// GET Profile API
+profileRouter.get("/profile/view",userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send(user);
@@ -16,5 +16,26 @@ profileRouter.get("/profile",userAuth, async (req, res) => {
 
 
 })
+
+// Profile edit API
+profileRouter.patch("/profile/edit", userAuth,async(req, res)=>{
+    try {
+       if(!validateProfileEditData(req)){
+            throw new Error("Invalid Edit Request");
+       }
+       const loggedInUser = req.user;
+       Object.keys(req.body).forEach((key) =>(loggedInUser[key] = req.body[key]));
+
+       await loggedInUser.save();
+
+       res.json({
+        messsage: `${loggedInUser.firstName} your profle updated successfully`, 
+        data: loggedInUser})
+
+    } catch (error) {
+        res.status(404).send("ERROR : " + error.message);
+    }
+})
+
 
 module.exports = profileRouter;
